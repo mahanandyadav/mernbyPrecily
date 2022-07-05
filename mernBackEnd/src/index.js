6ccc8664dd5c7a1cc791bbf6630a44d73f99a11e
Mahanand Yadav <1998355@tcs.com>
2022-07-05 22:12:46 +0530

    all done

--- mernBackEnd/src/index.js
+++ mernBackEnd/src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path')
 const session = require('express-session')
 const cookieParser = require('cookie-parser');
 const mongoDBStore = require('connect-mongodb-session')(session)
@@ -15,10 +16,11 @@ const store = new mongoDBStore({
     collection: "mySessions"
 })
 
-app.use(cookieParser("secret shshs"))
+app.use(cookieParser())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+/*
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", req.headers.origin);
     res.header("Access-Control-Allow-Headers", "*");
@@ -30,24 +32,34 @@ app.use((req, res, next) => {
         return res.status(200).send()
     }
     next();
-});
+})*/
 
 app.use(session({
-    secret: "secret shshs",
-    saveUninitialized: false,
+    secret: "mny",
+    saveUninitialized: true,
     cookie: {
         path: "/",//if / the cookies will be sent for all paths
         httpOnly: false,// if true, the cookie cannot be accessed from within the client-side javascript code.
-        secure: false,// true->cookie has to be sent over HTTPS
-        maxAge: 2*24 * 60 * 60 * 1000,
-        sameSite: 'None',//- `none` will set the `SameSite` attribute to `None` for an explicit cross-site cookie.
+        secure: false,// true->cookie has to be sent over HTTPS only
+        maxAge: 2 * 24 * 60 * 60 * 1000,
+        sameSite: true,//- `none` will set the `SameSite` attribute to `None` for an explicit cross-site cookie.
+        //for heroku sameStie:true/false works
     },
     store: store,
     resave: false,
 }))
 
-app.use(routes);
 
+
+if (process.env.NODE_ENV === 'production' || true) {
+    const loc = path.resolve(__dirname, '..', '..', 'mernFrontEnd', 'dist')
+    app.use(express.static(loc))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(loc, 'index.html'))
+    });
+    console.log('runnig build')
+}
+app.use(routes);
 const port = process.env.PORT || 3001;
 app.listen((port), (error) => {
     console.log(`lisning on port ${port}`)
--- mernBackEnd/src/routes/routes.js
+++ mernBackEnd/src/routes/routes.js
@@ -3,21 +3,19 @@ const User = require('../model/model');
 
 const routes = express.Router();
 
-/*
+
 routes.get('/', async (req, res, next) => {
     console.log('req.headers.cookie get')
-    console.log(req.headers.cookie)
     // console.log(req.headers.cookie.split(';')[1].split('=')[1])
     res.send({
         "post": req.session.postClick,
         "patch": req.session.patchClick
     })
-})*/
+})
 
 routes.post('/', async (req, res) => {
 
     console.log("req.headers.cookie post method")
-    console.log(req.headers)
 
     // console.log(req.headers.cookie.split(';')[1])
     const user = new User(req.body)
